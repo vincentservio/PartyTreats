490ddfd3c9a1976128f451f84f6ea4e2d5a36554
vincentservio <vincentservio@gmail.com>
2021-01-13 23:18:31 -0500

    form and back end connected

---
+++ frontend-partytreats/src/index.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", loadindex);
+
+function loadindex() {
+  const index = document.getElementById("index");
+
+  // let id = event.target.dataset.id;
+  //  let cardFormDiv = document.getElementById("name-form");
+  let html = `
+        <form>
+            Theme: <input type="text" id="theme" placeholder="Finding Nemo">
+            <br/>
+           Color: <input type="text" id="color" placeholder="Blue">
+             <br> 
+            Size:<input type="text" id="size" placeholder="Small">
+
+            <br> 
+            <select id="days">
+                <option>CupCakes</option>
+                <option>Cake</option>
+                <option>Cookies</option>
+            
+            <select>
+              <br> 
+            <input type="submit"> 
+        </form>
+        `;
+  index.innerHTML += html;
+  //  document.querySelector("form").addEventListener("submit", () => {
+  //    createChore(event, id);
+  //  }
+  //  );
+  fetch("http://localhost:3000/pastries")
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((pastry) => (index.innerHTML += `<div>${pastry.type_of}</div>`));
+    });
+}
